b94c8c14a11cc8236e49aa2b8bcf15c9b66c61f0
Arsh Bakshi <arsh.cp10@gmail.com>
2026-01-11 23:55:36 +0530

    Add files via upload

---
+++ RomanToInt.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h> 
+using namespace std;
+
+class Solution {
+public:
+    int romanToInt(string s) {
+        unordered_map<char, int> values = {
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000}
+        };
+
+        int result = 0;
+        for(int i = 0; i < s.size(); i++){
+            if (i + 1 < s.size() && values[s[i]] < values[s[i + 1]]) {
+                result -= values[s[i]];
+            } else {
+                result += values[s[i]];
+            }
+        }
+        return result;
+        
+    }
+};
+
+int main() {
+    Solution sol;
+    string s;
+    cout << "Enter a Roman numeral: ";
+    cin >> s;
+    int integerValue = sol.romanToInt(s);
+    cout << "The integer value is: " << integerValue << endl;
+    return 0;
+}
